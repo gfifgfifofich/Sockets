95826636e3d9d4a0f889fee4dd30bcda5af35114
gfifgfifofich <gfif90000@gmail.com>
2024-07-25 17:12:13 +0300

    init

---
+++ Source.cpp
@@ -0,0 +1,63 @@
+#include "Client.h"
+#include <iostream>
+#include <strstream>
+#include <chrono>
+#include <thread>
+#include <sys/time.h>
+
+std::string currentDateTime() {
+	time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+	struct tm tstruct;
+	char buf[80];
+	timeval start;
+	
+	gettimeofday(&start, NULL);
+	strftime(buf, sizeof(buf), "%Y-%m-%d %T", localtime(&now));
+	std::string s = buf;
+	s += ".";
+	int ms = start.tv_usec;
+	s += std::to_string(ms/1000);
+	return s;
+}
+
+int main(int argc, char** argv)
+{
+	SockClient cli;
+
+   	std::strstream ss;
+
+
+	if(argc<=2)
+	{
+		std::cout<<"Исполльзование:\n Client <name> <duration>\n Пример:\n Client Name1 1\n";
+		exit(1);
+	}
+
+	ss<<argv[1];
+	std::string name;
+	ss>>name;
+	ss.clear();
+
+	ss<<argv[2];
+	int d;
+	ss>>d;
+
+	cli.Connect(name, d,"127.0.0.1",30000);
+	
+	while (1)
+	{
+		while(1)
+		{
+			std::string logtext = '[' + currentDateTime() +"] " ; 
+			logtext += " ";
+			logtext += name;
+			if(!cli.Send(logtext.c_str()))
+			std::cout<<"failed ro send initial data to server";
+			std::chrono::seconds timespan(d);
+			std::this_thread::sleep_for(timespan);
+		}
+	}
+
+	cli.~SockClient();
+	return 0;
+}
